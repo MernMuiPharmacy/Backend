b5605d12729d6aa56383bb00bd804beaf7395b32
dev.Atef_Turki <56238500+devAtefturki@users.noreply.github.com>
2024-06-26 16:36:12 +0100

    Update index.js database credentials i hope noone leaks it (looking at you!!!!)

--- database/index.js
+++ database/index.js
@@ -1,9 +1,9 @@
 const mysql = require ("mysql2")
 var conn =mysql.createConnection({
-    host:'localhost',
-    user:'root',
-    port:3306,
-    password:'root',
+    host:'mysql-3783e59f-compliments.i.aivencloud.com',
+    user:'avnadmin',
+    port:23612,
+    password:'AVNS_oD0e-ZBomT4mHL_JpTx',
     database:'pharma'
 })
 conn.connect((err)=>{
